2dcf1d2c8a497d329478f87480c457b28ae0409e
mohtashim-khan <moto_khan@hotmail.com>
2021-03-30 23:16:45 -0600

    Added Furniture and CalculateCombinations file

---
+++ src/CalculateCombinations.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author moto_
+ *
+ */
+public class CalculateCombinations {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Furniture.java
@@ -0,0 +1,15 @@
+
+abstract class Furniture 
+{
+	private String iD;
+	private String type;
+	private int price;
+	private String manuID;
+	
+	public void calculatePrice();
+	
+	
+	
+	
+
+}
