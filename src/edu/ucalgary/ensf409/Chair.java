b0f8f462dd32f41f57c44568967200ea9b23705c
Mackenzie-Chak <77872708+Mackenzie-Chak@users.noreply.github.com>
2021-04-03 19:24:27 -0600

    Added package declarations and changed chair code

---
+++ src/edu/ucalgary/ensf409/Chair.java
@@ -0,0 +1,54 @@
+package edu.ucalgary.ensf409;
+
+public class Chair extends Furniture {
+	private String arms;
+	private String seat;
+	private String cushion;
+
+	
+	
+
+	
+	//0 arg constructor
+	//need to pull data from SQL database and populate chairInfo array
+	
+	
+// create database class
+	
+	public void selectChairs() {
+		
+	}
+	
+ // leave for now. Might get rid later.
+	
+	public Chair(String iD, String type, int price, String manuID, String arms, String seat, String cushion) {
+		super(iD, type, price, manuID);
+		// TODO Auto-generated constructor stub
+		this.arms = arms;
+		this.seat = seat;
+		this.cushion = cushion;
+	}
+
+	public String getArms() {
+		return this.arms;
+	}
+	public String getSeat() {
+		return this.seat;
+	}
+	public String getCushion() {
+		return this.cushion;
+	}
+	
+
+	public void setArms(String arms) {
+		this.arms = arms;
+	}
+	public void setSeat(String seat) {
+		this.seat = seat;
+	}
+	public void setCushion(String cushion) {
+		this.cushion = cushion;
+	}
+	
+
+}
