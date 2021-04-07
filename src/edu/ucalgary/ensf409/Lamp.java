20e85789e8c06391630a39c188571cee2fe9e6a9
Mackenzie-Chak <77872708+Mackenzie-Chak@users.noreply.github.com>
2021-04-06 22:59:22 -0600

    Added commenting/documentation + new UI Class
    Prelim UI Class.
    Can develop more after meeting tomorrow.

--- src/edu/ucalgary/ensf409/Lamp.java
+++ src/edu/ucalgary/ensf409/Lamp.java
@@ -1,12 +1,25 @@
+/**
+ *@author	Group 73 (Mackenzie,Mohtashim, Ritwik, Usman) <a>
+ *href="mailto:mohtashim.khan@ucalgary.ca">mohtashim.khan@ucalgary.ca</a>
+ *Project Manager: Mohtashim Khan
+ *@version 1.0
+ *@since 1.0
+ *
+ *Filing is a basic class that stores all the string variables from the
+ *SQL database provided by the course instructors.
+ *
+ */
+
+//Package declaration
 package edu.ucalgary.ensf409;
 
 public class Lamp extends Furniture {
 	
-
-
+	//Class variables
 	private String base;
 	private String bulb;
 	
+	//Constructor
 	public Lamp(String iD, String type, String base, String bulb, String price, String manuID) {
 		
 		super(iD, type, price, manuID);
@@ -14,22 +27,23 @@ public class Lamp extends Furniture {
 		this.bulb = bulb;
 
 	}
-
+	
+	//Getter
 	public String getBase() {
 		return this.base;
 	}
+	//Getter
 	public String getBulb() {
 		return this.bulb;
 	}
-// Setters - 
 	
+	//Setter	
 	public void setBase(String base) {
 		this.base = base;
 	}
+	//Setter
 	public void setBulb(String bulb) {
 		this.bulb = bulb;
 	}
-	
-
-
 }
+//End class definition
